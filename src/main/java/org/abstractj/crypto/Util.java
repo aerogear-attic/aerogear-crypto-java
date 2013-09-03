65cb731cf880c2fa105f941d6d8808f9b75162d5
Bruno Oliveira <bruno@abstractj.org>
2013-09-03 13:31:50 -0300

    Fixing stupity on checks

--- src/main/java/org/abstractj/crypto/Util.java
+++ src/main/java/org/abstractj/crypto/Util.java
@@ -3,8 +3,8 @@ package org.abstractj.crypto;
 public class Util {
 
     public static byte[] checkLength(byte[] data, int size) {
-        if (data == null || data.length != size)
-            throw new RuntimeException("Invalid size: " + data.length);
+        if (data == null || data.length < size)
+            throw new RuntimeException("Invalid length: " + data.length);
         return data;
     }
 
