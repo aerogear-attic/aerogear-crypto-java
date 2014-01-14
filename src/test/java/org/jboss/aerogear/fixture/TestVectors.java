ca9e177caa41f2ae88fd1d20553b2d26bfb426ba
Bruno Oliveira <bruno@abstractj.org>
2014-01-14 14:01:57 -0500

    Hashing support for SHA-256 and SHA-512

--- src/test/java/org/jboss/aerogear/fixture/TestVectors.java
+++ src/test/java/org/jboss/aerogear/fixture/TestVectors.java
@@ -57,4 +57,17 @@ public class TestVectors {
             "dd04a8646d71873b77b221998a81922d8105f892316369d5224c9983372d2313" +
             "c6b1f4556ea26ba49d46e8b561e0fc76633ac9766e68e21fba7edca93c4c7460" +
             "376d7f3ac22ff372c18f613f2ae2e856af40";
+
+
+    //SHA256 test vectors
+    public static final String SHA256_MESSAGE = "My Bonnie lies over the ocean, my Bonnie lies over the sea";
+    public static final String SHA256_DIGEST = "d281d10296b7bde20df3f3f4a6d1bdb513f4aa4ccb0048c7b2f7f5786b4bcb77";
+    public static final String SHA256_DIGEST_EMPTY_STRING = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+
+    //SHA512 test vectors
+    public static final String SHA512_MESSAGE = "My Bonnie lies over the ocean, Oh bring back my Bonnie to me";
+    public static final String SHA512_DIGEST = "2823e0373001b5f3aa6db57d07bc588324917fc221dd27975613942d7f2e19bf4" +
+            "44654c8b9f4f9cb908ef15f2304522e60e9ced3fdec66e34bc2afb52be6ad1c";
+    public static final String SHA512_DIGEST_EMPTY_STRING = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921" +
+            "d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
 }
